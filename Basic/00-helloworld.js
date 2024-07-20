0b7cc7291ff19c15a0d74c4356709fa63fc2dbea
Pedro <sanvar32@hotmail.com>
2024-07-20 10:53:14 +0200

    Commit inicial

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,10 @@
+/* console.log('Hello, World!');
+console.log("Hello, World!"); */
+
+// Operaciones
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 % 2);
+console.log(275 ** 275);
---
+++ Basic/01-variables.js
@@ -0,0 +1,16 @@
+// Variables en desuso
+
+// var
+var helloWorld = 'Hello, World!';
+console.log(helloWorld);
+
+helloWorld = 'Hello, Universe!';
+console.log(helloWorld);
+
+// let
+let helloWorld2 = 'Hello, World2!';
+console.log(helloWorld2);
+
+// const no pueden ser reasignados
+const helloWorld3 = 'Hello, World3!';
+console.log(helloWorld3);
